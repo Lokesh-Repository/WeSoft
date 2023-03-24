a71887870bac87d65840c238240b85028e4b7d10
Lokesh-Repository <120589943+Lokesh-Repository@users.noreply.github.com>
2023-03-24 21:08:33 +0530

    Improved User Interface Of The Mail
    Improver The User Experience By Adding Some Stuffs To The Message Which Was Being Conveyed In The Password Reset Mail.

--- forgotpass.php
+++ forgotpass.php
@@ -15,27 +15,31 @@ function sendMail($email, $reset_token)
         $mail->isSMTP(); //Send using SMTP
         $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
         $mail->SMTPAuth = true; //Enable SMTP authentication
-        $mail->Username = ''; //SMTP username (Add Your Username)
-        $mail->Password = ''; //SMTP password (Add Your Password)
+        $mail->Username = 'wesoft.rf.gd@gmail.com'; //SMTP username
+        $mail->Password = 'rdbqhsnfadeekzwi'; //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
         $mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
-        $mail->setFrom('wesoft.rf.gd@gmail.com', 'Password Change Request from WeSoft');
+        $mail->setFrom('wesoft.rf.gd@gmail.com', 'Password Change Request From WeSoft');
         $mail->addAddress($email); //Add a recipient
 
         //Content
         $mail->isHTML(true); //Set email format to HTML
         $mail->Subject = 'Reset Your We Soft Account Password';
-        $mail->Body = " Hello There,  <br>
-                        We Assume You Made A Password Change Request! If Yes Check Below! Else <b><u>DON't DO ANYTHING DUMB BY CLICKING ON THE LINK BELOW</u></b>
-                       Click the link below : <br>
-                       <a href='http://wesoft.rf.gd/updatepassword.php?email=$email&reset_token=$reset_token'>Click Here To Reset Your Password</a> <br>
-                       Else just Copy paste The Below Link<br>
-                       <p>http://wesoft.rf.gd/updatepassword.php?email=$email&reset_token=$reset_token</p>"
-                       ;
-        $mail->AltBody = 'yeah we have sent you an password reset link (you are seeing this text as you are non-html customer)';
+        $mail->Body = " Hello There,  <br> <br> <br>
+                        We See That You Made A Password Change Request! If It Was Really You,Click Below! To Reset Your Password,<br><br><center>
+                        <a> <button href='http://wesoft.rf.gd/updatepassword.php?email=$email&reset_token=$reset_token' style='background-color: #002bff; border-radius: 6px; border-width:0; align-items:center; outline: none; text-align: center; width: fit-content;'> Reset Password </button> </a> </center> <br>
+                         Above Button Not Working? just Copy And Paste The Below Link In Your Favourite Browser:
+                         <p>http://wesoft.rf.gd/updatepassword.php?email=$email&reset_token=$reset_token</p>  <br><br><br><br>
 
+                       If You Did Not Initiate This Request DON't DO ANYTHING STUPID. LIKE, CLICKING ON THE LINK/THE BUTTON ABOVE<br>
+                       
+                       Believe You Did Not Make This Request?  <br> Don't Panic/Don't Curse Any Of Our Developer's/Don't Try To Delete Your WeSoft Account/Don't Think Your Data is Un-Safe (Between If You Are A Real Human You'r Data Is Probablly Already On Web).
+                       <br><center><b><u>OR<u><b></center><br>
+                       Just Mail To Us on <center><b><u>wesoft.rf.gd@gmail.com<u><b></center> <b> We Will Investigate And Take The Best Course Of Action. <br><br>AGAIN YOUR DATA IS ABSOLUTELLY SAFE WITH US.WE ARE NOT FOOL TO GIVE YOUR DATA FOR FREE....</b>";
+
+        $mail->AltBody = 'yeah we have sent you an password reset link (you are seeing this text as you are non-html customer)';
         $mail->send();
         return true;
     } catch (Exception $e) {
@@ -61,7 +65,7 @@ if (isset($_POST['send-link-mail'])) {
             } else {
                 echo "
             <script>
-            alert('Ahhhh, CRAP!! Something Went Wrong, try again later');
+            alert('Ahhhh, CRAPP!! Something Went Wrong, try again later');
             window.location.href='forgotpassword.html'
             </script>
             ";
